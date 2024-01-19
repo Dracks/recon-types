95fd5b931796873c2b3ec7524d910e74983cebfb
darkbladecr <smig88@gmail.com>
2024-01-19 18:24:35 -0500

    feat(damage): add secondary damage chart and readable xlsx

--- validate.sh
+++ validate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ./validator/yajsv.darwin.amd64 -s ./schema/unit.json 'units/*.json'
 ./validator/yajsv.darwin.amd64 -s ./schema/dmg_chart.json damage/primary.json
+./validator/yajsv.darwin.amd64 -s ./schema/dmg_chart.json damage/secondary.json
 ./validator/yajsv.darwin.amd64 -s ./schema/movement_chart.json movement/chart.json
